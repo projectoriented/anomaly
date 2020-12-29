0ca317cda841376d3aa28fe9a1d20cd9bf4a214a
Mei Wu <savantmei@gmail.com>
2020-12-29 16:25:38 +0100

    edits

--- scripts/rename_files.sh
+++ scripts/rename_files.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 path=$1
 rule=$2
-capture_group=$3
+lane=$3
+capture_group=$4
 
-find $path -name "*$rule.*" | while read line; 
+find $path -type f -name "$lane\_*$capture_group\_*$rule*" | while read line; 
 do
-    name=$(echo $line | sed -E "s/(.*\/._?).*($capture_group)/\1\2/g");
+    name=$(echo $line | sed -E "s/(.*\/$lane.).*($capture_group)/\1\2/g");
     mv $line $name;
 done
