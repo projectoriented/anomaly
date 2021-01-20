52dd55bd77689106eb73129c89c190fbc26b2e7e
Mei Wu <savantmei@gmail.com>
2021-01-20 21:32:47 +0100

    v1

--- scripts/generate_drop_sample_annot.py
+++ scripts/generate_drop_sample_annot.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 samples = pd.read_table(snakemake.input[0], dtype=str)
+meta = pd.read_table(snakemake.input[1], dtype=str).fillna("").set_index("sample_id")
+
 d = {
     'RNA_ID': [],
     'RNA_BAM_FILE': [],
@@ -34,10 +36,28 @@ for row in samples.itertuples(index=False):
     d['RNA_ID'].append(basename_search)
     d['RNA_BAM_FILE'].append(abs_path)
 
-    modified = remove_string_rna(row.sample)[-2:]
-    d['TISSUE'].append('Fibroblast') if row.sample == '164-I-2A' or row.sample == '90-I-1A' else d['TISSUE'].append('Blood')
-    d['SEX'].append('FEMALE') if '2' in modified else d['SEX'].append('MALE')
-    d['PHENOTYPE'].append('UNAFFECTED') if 'U' in modified else d['PHENOTYPE'].append('AFFECTED')
+    modified = remove_string_rna(row.sample)
+
+    dna_sample_id = meta.loc[remove_string_rna(row.sample)]['dna_sample_id']
+    d['DNA_ID'].append(dna_sample_id)
+
+    dna_case = meta.loc[remove_string_rna(row.sample)]['dna_case']
+
+    if not row.sample.isalnum() and dna_case:
+        dir_name = row.sample.split("-")[0]
+        vcf_path = f"/home/proj/development/rare-disease/rna_data/{dir_name}/dna_vcf/{dna_case}_gatkcomb.vcf.gz"
+    elif not dna_case:
+        vcf_path = ""
+    else:  # e.g. ACC3285A1 is alnum.
+        dir_name = "15138"
+        vcf_path = f"/home/proj/development/rare-disease/rna_data/{dir_name}/dna_vcf/{dna_case}_gatkcomb.vcf.gz"
+
+    d['DNA_VCF_FILE'].append(vcf_path)
+
+    d['TISSUE'].append('Fibroblast') if row.sample == '164-I-2A' or row.sample == '90-I-1A' or '15001' in row.sample else d['TISSUE'].append(
+        'Blood')
+    d['SEX'].append('FEMALE') if '2' in modified[-2:] else d['SEX'].append('MALE')
+    d['PHENOTYPE'].append('UNAFFECTED') if 'U' in modified[-2:] else d['PHENOTYPE'].append('AFFECTED')
 
 d['DROP_GROUP'] = d['TISSUE']
 
